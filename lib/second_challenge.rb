e3b3f94c6493a92123c9b8b17b3d285204ce52c6
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-16 22:06:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+   :montague => {
       :patriarch => {},
       :matriarch => {},
-      
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
   
 end
